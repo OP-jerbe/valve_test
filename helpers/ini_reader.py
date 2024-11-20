ed9e7d006e50404573b564eed5fb33923020e426
OP-jerbe <j.erbe@oregon-physics.com>
2024-11-20 13:55:19 -0800

    Created a helper module with an ini_reader.py file hold the functions used to read in the ini file contents.

---
+++ helpers/ini_reader.py
@@ -0,0 +1,9 @@
+import configparser as cp
+
+def load_ini(ini_file: str) -> cp.ConfigParser:
+     config_data = cp.ConfigParser()
+     config_data.read(ini_file)
+     return config_data
+
+def find_comport(config_data: cp.ConfigParser, header: str) -> str:
+    return config_data.get(header, 'com_port')
