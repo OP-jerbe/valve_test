b6a386b689d1d403644afad59a30279fcb9539d7
OP-jerbe <j.erbe@oregon-physics.com>
2024-11-20 11:01:06 -0800

    Added functionality to read in ini file data so that COM ports for the pressure gauge and motor can be set.

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
+import configparser as cp
 import api.motor as mot
 import gui.gui as gui
 from api.pfeiffer_tpg26x import TPG261 as tpg
 
 
+def load_ini(ini_file: str) -> cp.ConfigParser:
+     config_data = cp.ConfigParser()
+     config_data.read(ini_file)
+     return config_data
+
+def find_comport(config_data: cp.ConfigParser, header: str) -> str:
+    return config_data.get(header, 'com_port')
+
+
+
+
 def main() -> None:
-    pass
+    ini_file = 'valve_test.ini'
+    config_data = load_ini(ini_file)
+    motor_com_port = find_comport(config_data, 'Motor')
+    p_gauge_com_port = find_comport(config_data, 'Pressure_Gauge')
+    print(f'{motor_com_port = }')
+    print(f'{p_gauge_com_port = }')
 
 if __name__ == '__main__':
     main()
