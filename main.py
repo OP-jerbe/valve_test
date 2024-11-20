716b10a0567cefd7c8c33199d91178438541d7ca
OP-jerbe <j.erbe@oregon-physics.com>
2024-11-20 14:26:00 -0800

    Created App class

--- main.py
+++ main.py
@@ -4,6 +4,21 @@ from api.motor import MotorController as mc
 from gui.gui import QApplication, MainWindow
 from api.pfeiffer_tpg26x import TPG261 as tpg
 
+VERSION = '1.0'
+
+class App:
+    def __init__(self):
+        self.app = QApplication([])
+        self.gui = MainWindow()
+        self.gui.setWindowTitle(f'Automated Valve Test v{VERSION}')
+
+
+        self.gui.show()
+
+    def run(self) -> None:
+        exit_code: int = self.app.exec()
+        sys.exit(exit_code)
+
 
 def main() -> None:
     ini_file = 'valve_test.ini'
@@ -19,6 +34,10 @@ def main() -> None:
     window.show()
     sys.exit(app.exec())
 
+def main2() -> None:
+    app = App()
+    app.run()
+
 if __name__ == '__main__':
-    main()
+    main2()
 
