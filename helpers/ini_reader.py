2582e8327e8944d010f8d8ee51ed511df925b326
OP-jerbe <j.erbe@oregon-physics.com>
2024-11-20 15:13:17 -0800

    Changed it back to returning a string rather than an int

--- helpers/ini_reader.py
+++ helpers/ini_reader.py
@@ -5,5 +5,5 @@ def load_ini(ini_file: str) -> cp.ConfigParser:
      config_data.read(ini_file)
      return config_data
 
-def find_comport(config_data: cp.ConfigParser, header: str) -> int:
-    return int(config_data.get(header, 'com_port'))
+def find_comport(config_data: cp.ConfigParser, header: str) -> str:
+    return config_data.get(header, 'com_port')
