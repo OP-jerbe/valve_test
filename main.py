7ecdedd972d3bca23f3ac11346e63e4d20a99451
OP-jerbe <j.erbe@oregon-physics.com>
2024-11-21 10:10:54 -0800

    Made some functions to control the motor.

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class App:
         self.gui = MainWindow()
         self.gui.setWindowTitle(f'Automated Valve Test v{VERSION}')
 
-        self.motor = MotorController(port=motor_com_port)
+        self.motor = self.connect_to_motor(motor_com_port, 100, 50)
         self.pressure_gauge = TPG261(port=pressure_gauge_com_port)
 
         self.timer = QTimer()
@@ -26,12 +26,39 @@ class App:
         self.gui.open_button.released.connect(self.stop_motor)
         self.gui.close_button.pressed.connect(self.start_clockwise_motor)
         self.gui.close_button.released.connect(self.stop_motor)
+        self.gui.home_button.clicked.connect(self.home_button_handler)
+        self.gui.set_zero_button.clicked.connect(self.set_zero_button_handler)
+        self.gui.go_to_position_button.clicked.connect(self.go_to_position_button_handler)
 
         self.gui.show()
 
-    def connect_to_motor(self, com_port: int) -> None:
-        
-        
+    def connect_to_motor(self, com_port: str, running_current: int, holding_current: int) -> MotorController:
+        motor = MotorController(port=com_port)
+        motor.set_current(running_current, holding_current)
+        return motor
+
+    def home_button_handler(self) -> None:
+        self.motor.home_motor()
+        position = self.motor.query_position()
+        self.gui.actual_position_reading.setText(position)
+        # query motor for position and set self.gui.actual_position_reading text to position reading.
+    
+    def set_zero_button_handler(self) -> None:
+        self.motor.set_zero()
+        position = self.motor.query_position()
+        self.gui.actual_position_reading.setText(position)
+        # query motor for position and set self.gui.actual_position_reading text to position reading.
+
+    def go_to_position_button_handler(self) -> None:
+        target_position = self.gui.go_to_position_input.text()
+        self.motor.move_absolute(target_position)
+        self.gui.go_to_position_input.clear()
+        # query motor for position and set self.gui.actual_position_reading text to position reading.
+
+    def go_to_position(self, target_position) -> None:
+        # Code for moving the motor to target position goes here
+        self.gui.actual_position_reading.setText(target_position)
+        print(f'Motor went to {target_position}')
 
     def start_clockwise_motor(self) -> None:
         self.direction = 'clockwise'
