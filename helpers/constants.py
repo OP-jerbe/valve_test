0bea2868b87f26659595d01d5caf5c2995329604
OP-jerbe <j.erbe@oregon-physics.com>
2024-12-11 09:29:15 -0800

    Changed microsteps per step of motor to be 1 so that the microsteps_per_rev is 200.

--- helpers/constants.py
+++ helpers/constants.py
@@ -5,12 +5,16 @@ except:
 
 config_data = load_ini('valve_test.ini')
 
+
 VERSION: str = '1.0'
-STEPS_PER_REV: int = 200
-MICROSTEPS_PER_STEP: int = 256
+
+# Motor control constants
+STEPS_PER_REV: int = 200 # Set by motor design do not change
+MICROSTEPS_PER_STEP: int = 1 # acceptable values are: 1, 2, 4, 8, 16, 32, 64, 128, 256
 MICROSTEPS_PER_REV: int = STEPS_PER_REV * MICROSTEPS_PER_STEP
 MAX_VALVE_TURNS: int = 22
 
+# Valve test constants
 VALVE_STEP_SIZE: float = float(find_constant(config_data, 'VALVE_STEP_SIZE'))
 MOTOR_STEP_SIZE: int = int(VALVE_STEP_SIZE * MICROSTEPS_PER_REV)
 HOLD_TIME: int = int(find_constant(config_data, 'HOLD_TIME')) # the number of seconds to wait for stability
