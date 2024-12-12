c2a9e2ab29742f498e85daff4f151a7c02f88b99
OP-jerbe <j.erbe@oregon-physics.com>
2024-12-12 07:30:49 -0800

    changed time to wait at top of valve test to 5 seconds

--- helpers/valve_test.py
+++ helpers/valve_test.py
@@ -99,13 +99,12 @@ class ValveTest:
     def _check_if_valve_has_reached_turn_around_point(self) -> None:
         if self._pressure_is_above_PRESSURE_TURN_POINT() and self._valve_is_opening():
                 self._open_valve(MICROSTEPS_PER_REV) # open valve one full turn
-                self.pause(2.5)
+                self.pause(5)
                 self._log_turns_and_pressure(self.valve_position, self.pressure)
                 self.direction = 'down'
-                self.pause(2.5)
                 self._log_turns_and_pressure(self.valve_position, self.pressure)
                 self._close_valve(MICROSTEPS_PER_REV) # close valve one full turn
-                self.pause(2.5)
+                self.pause(5)
 
     def _check_if_valve_test_needs_to_stop(self) -> None:
         if (self._valve_is_at_zero() or self._pressure_is_below_base_pressure()) and self._valve_is_closing():
