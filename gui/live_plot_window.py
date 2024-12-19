46d836b33f1ce0f86fbd0d784eda87cefc863d18
OP-jerbe <j.erbe@oregon-physics.com>
2024-12-19 08:46:10 -0800

    updated size of fonts and live pressure vs turns figure size.

--- gui/live_plot_window.py
+++ gui/live_plot_window.py
@@ -13,19 +13,20 @@ class LivePlotWindow(QDialog):
         self.base_pressure: str = base_pressure
 
         # Create matplotlib figure and canvas
-        self.fig = plt.figure(dpi=200, frameon=True, edgecolor='k', linewidth=2, figsize=(11*0.6,8*0.5))
+        self.fig = plt.figure(dpi=200, frameon=True, edgecolor='k', linewidth=2, figsize=(11*0.6,8*0.7))
         self.ax = self.fig.add_subplot(1, 1, 1)
         self.ax.set_title(f'VAT Valve #{self.serial_number}({self.rework_letter})')
-        self.ax.set_xlabel("Leak Valve Turns")
-        self.ax.set_ylabel("Pressure (mBar)")
+        self.ax.set_xlabel("Leak Valve Turns", fontsize=7)
+        self.ax.set_ylabel("Pressure (mBar)", fontsize=7)
         self.ax.grid()
         self.canvas = FigureCanvas(self.fig)
 
         # Set fixed axis limits and scaling
         self.ax.set_xlim(-0.5, 12.5)
         self.ax.set_ylim(1e-8, 1e-3)
-        self.ax.tick_params(axis='both', labelsize=8)
         self.ax.set_yscale('log')
+        self.ax.tick_params(axis='both', labelsize=8)
+        self.ax.set_xticks(range(0,13))
 
         # Add canvas to the window
         layout = QVBoxLayout()
@@ -41,15 +42,16 @@ class LivePlotWindow(QDialog):
         # Reapply fixed axes settings
         self.ax.set_xlim(-0.5, 12.5)
         self.ax.set_ylim(1e-8, 1e-3)
-        self.ax.tick_params(axis='both', labelsize=8)
         self.ax.set_yscale('log')
+        self.ax.tick_params(axis='both', labelsize=8)
+        self.ax.set_xticks(range(0,13))
 
         # Plot the data
         self.ax.plot(valve_turns_up, pressure_up, marker="o", markersize=2, label="Opening", color='tab:blue')
         self.ax.plot(valve_turns_down, pressure_down, marker="o", markersize=2, label="Closing", color='lightskyblue')
         self.ax.grid()
         self.ax.set_title(f'VAT Valve #{self.serial_number}({self.rework_letter})')
-        self.ax.set_xlabel("Leak Valve Turns")
-        self.ax.set_ylabel("Pressure (mBar)")
+        self.ax.set_xlabel("Leak Valve Turns", fontsize=7)
+        self.ax.set_ylabel("Pressure (mBar)", fontsize=7)
         self.ax.legend(fontsize=5)
         self.canvas.draw()
