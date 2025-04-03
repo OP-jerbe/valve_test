6e055ef1faff0c7dc308b7835e0c23e6b22abf46
OP-jerbe <j.erbe@oregon-physics.com>
2025-04-03 14:42:16 -0700

    puts back in QTimer import.

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from gui.normalized_plot_window import NormalizedPlotWindow
 from helpers.constants import VERSION, MICROSTEPS_PER_STEP, MICROSTEPS_PER_REV, MAX_VALVE_TURNS
 from helpers.ini_reader import load_ini, find_comport, find_selection
 from helpers.valve_test import ValveTest
+from PySide6.QtCore import QTimer # do not delete
 
 
 class App:
